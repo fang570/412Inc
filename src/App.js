c7b983b8815b2519a66d402772eb492a8c8f51de
Caleb Bucci <booch2nd@gmail.com>
2022-03-15 19:58:01 -0400

    Testing changes to page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Sup Caleb. Deploying changes.
+          Sup Caleb. Deploying changes. Sup George. Got it.
         </p>
         <a
           className="App-link"
